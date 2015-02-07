ab651d3bc2a50ae9bd62aef92045eebb620e2c4e
tayfun biyikoglu <rain@tayfuns-MacBook-Pro.local>
2015-02-07 21:28:15 +0100

    Initial Commit

---
+++ ceb_tea/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ceb_tea
+//
+//  Created by tayfun biyikoglu on 07/02/15.
+//  Copyright (c) 2015 tayfun biyikoglu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
